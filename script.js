a1cd1befb49ea3b8ebaf2fd3e187084facffc649
MohdMuttavaquil <mmuttavaquil@gmail.com>
2026-03-19 13:16:06 +0530

    Add cors

--- script.js
+++ script.js
@@ -15,12 +15,8 @@ const PORT  = 4000
 const server = createServer(app)
 const io = new Server(server, {
   cors:{
-    origin: [ "http://localhost:5173",
-   "https://chat-app-1-h1qb.onrender.com" ],
-    methods: ["GET", "POST"],
-  },
-   transports: ["websocket", "polling"]
-})
+    origin: "https://chat-app-1-h1qb.onrender.com"
+  }})
 
 // Database connaction
 dbConnect()
